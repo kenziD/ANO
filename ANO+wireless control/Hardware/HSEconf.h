b621f3a88e48bc51cfb4eb20524e8fae9a0a15ea
kenzi <1369811517@qq.com>
2015-07-20 21:25:04 +0800

    adjust PID

---
+++ ANO+wireless control/Hardware/HSEconf.h
@@ -0,0 +1,6 @@
+#ifndef __HSEconf_H
+#define __HSEconf_H
+#include "config.h"
+void RCC_HSE_Configuration(void);
+
+#endif
---
+++ ANO+wireless control/Hardware/config.h
@@ -0,0 +1,27 @@
+#ifndef __CONFIG_H_
+#define	__CONFIG_H_
+
+#include "stm32f10x.h"
+#include "delay.h"
+#include "usart.h"
+#include "LED.h"
+#include "IIC.h"
+#include "MPU6050.h"
+#include "IMU.h"
+#include "HSEconf.h"
+#include "MOTOR.h"
+#include "spi.h"
+#include "24l01.h"
+#include "PID.h"
+#include <math.h>
+#include "stm32f10x_tim.h"
+#include "stdint.h"
+#include <string.h>
+#endif /* __CONFIG_H_ */
+
+
+//------------------End of File----------------------------
+
+
+
+
