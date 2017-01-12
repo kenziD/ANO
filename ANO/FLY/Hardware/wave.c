4238ad48a7b5a8f26dca6cffa0841fb0f4fb3480
kenzi <1369811517@qq.com>
2017-01-12 15:13:47 +0800

    改回来42hz movefilter。好像用低通滤波稳得很慢反正有问题

--- ANO/FLY/Hardware/wave.c
+++ ANO/FLY/Hardware/wave.c
@@ -100,11 +100,11 @@ void Data_Transfer()
 		//send_wave(32);
 
 		//Version2
-		//send_senserV2(fACCEL_X, fACCEL_Y,fACCEL_Z, fGYRO_X, fGYRO_Y,fGYRO_Z, 0x00,0x00,0x00);
-		//send_wave(23);
-		
-		send_senserV2(AccFilterOut.x, AccFilterOut.y,AccFilterOut.z, fGYRO_X, fGYRO_Y,fGYRO_Z, fACCEL_X,fACCEL_Y,fACCEL_Z);
+		send_senserV2(fACCEL_X, fACCEL_Y,fACCEL_Z, fGYRO_X, fGYRO_Y,fGYRO_Z, 0x00,0x00,0x00);
 		send_wave(23);
+		
+		//send_senserV2(AccFilterOut.x, AccFilterOut.y,AccFilterOut.z, fGYRO_X, fGYRO_Y,fGYRO_Z, fACCEL_X,fACCEL_Y,fACCEL_Z);
+		//send_wave(23);
 		
 	}
 	else if(send_Status)
