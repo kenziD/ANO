41bbb688fdbd83fe9ce056f9ecbf157d3e2a40e6
kenzi <1369811517@qq.com>
2017-03-02 14:06:55 +0800

    效果显著。采样频率一下子提升到了400hz

--- ANO/FLY/User/main.c
+++ ANO/FLY/User/main.c
@@ -67,68 +67,68 @@ int main(void)
 	//setCutOffFrequency(500,10);//T=0.02ms fs=1/T=500,Fcut=28Hz;
 	while (1)
 	{
-		if (getMpu6050Data == 1)//1ms period
-		{
-			att_cnt++;
+		//if (getMpu6050Data == 1)//1ms period
+		//{
+			//att_cnt++;
 			Read_Mpu6050(); 
 			Mpu6050_Analyze();
 			//moveFilterAccData(fACCEL_X,fACCEL_Y,fACCEL_Z,&ACC_AVG );
-			moveFilterAccData(fACCEL_X_zhihu_pix,fACCEL_Y_zhihu_pix,fACCEL_Z_zhihu_pix,&ACC_AVG );
+			//moveFilterAccData(fACCEL_X_zhihu_pix,fACCEL_Y_zhihu_pix,fACCEL_Z_zhihu_pix,&ACC_AVG );
 		
-			getMpu6050Data = 0;
-			if(att_cnt==2)
-			{
-				att_cnt = 0;
-				outterPid_cnt++;
+			//getMpu6050Data = 0;
+			
+			//if(att_cnt==2)
+			//{
+				//att_cnt = 0;
+				//outterPid_cnt++;
 				//ButterWorthLPF_2order(&MPU_ACC_READ,&AccFilterOut);
 				//LED2_ON;
-				IMU_Quateration_Update((float)fGYRO_X , (float)fGYRO_Y , (float)fGYRO_Z , (float)ACC_AVG.x, (float)ACC_AVG.y, (float)ACC_AVG.z,&outAngle);
+				//IMU_Quateration_Update((float)fGYRO_X , (float)fGYRO_Y , (float)fGYRO_Z , (float)ACC_AVG.x, (float)ACC_AVG.y, (float)ACC_AVG.z,&outAngle);
 				//IMU_Quateration_Update((float)fGYRO_X , (float)fGYRO_Y , (float)fGYRO_Z , (float)fACCEL_X, (float)fACCEL_Y, (float)fACCEL_Z,&outAngle);
 				//IMU_Quateration_Update((float)fGYRO_X , (float)fGYRO_Y , (float)fGYRO_Z , (float)AccFilterOut.x, (float)AccFilterOut.y, (float)AccFilterOut.z,&outAngle);
-				desireAngle.roll = (Rc_Data.aux1-1500)/100.0f+(Rc_Data.roll-1500)/13.0f;
-				desireAngle.pitch = (Rc_Data.aux2-1500)/100.0f+(Rc_Data.pitch-1500)/13.0f;
+				//desireAngle.roll = (Rc_Data.aux1-1500)/100.0f+(Rc_Data.roll-1500)/13.0f;
+				//desireAngle.pitch = (Rc_Data.aux2-1500)/100.0f+(Rc_Data.pitch-1500)/13.0f;
 				
 				//desireAngle.roll = 0;
 				//desireAngle.pitch = 0;
 				gyroControl(Rc_Data.throttle);
-				//4ms运行一次内环控制。
-				if(outterPid_cnt==2)//4ms
-				{
-					outterPid_cnt = 0;
-					angleControl(&outAngle,&desireAngle,Rc_Data.throttle);
-				}
+//				//4ms运行一次内环控制。
+//				if(outterPid_cnt==2)//4ms
+//				{
+//					outterPid_cnt = 0;
+//					angleControl(&outAngle,&desireAngle,Rc_Data.throttle);
+//				}
 				//ControlPID(Rc_Data.throttle);
-				calculateAngle = 0;
+				//calculateAngle = 0;
 				//LED2_OFF;
-			}
-		}
-
+			//}
+		//}
 		if (sendData == 1)//1ms period
 		{
 				send_Senser_cnt++;
 				send_Status_cnt++;
 				send_desirePIDAngle_cnt++;
 				send_RcData_cnt++;
-				if(send_Senser_cnt==10)//5ms
+				if(send_Senser_cnt==1)//5ms
 				{
 					send_Senser = 1;
 					send_Senser_cnt = 0;
 				}
-				if(send_Status_cnt==15)
+				if(send_Status_cnt==5)
 				{
 					send_Status = 1;
 					send_Status_cnt = 0;
 				}
-				if(send_RcData_cnt==20)
-				{
-					send_RcData = 1;
-					send_RcData_cnt = 0;
-				}
-				if(send_desirePIDAngle_cnt==25)//5ms
-				{
-					send_desirePIDAngle = 1;
-					send_desirePIDAngle_cnt = 0;
-				}
+				//if(send_RcData_cnt==20)
+				//{
+				//	send_RcData = 1;
+					//send_RcData_cnt = 0;
+				//}
+				//if(send_desirePIDAngle_cnt==25)//5ms
+				//{
+				//	send_desirePIDAngle = 1;
+				//	send_desirePIDAngle_cnt = 0;
+				//}
 			sendData = 0;
 		}
 		////Uart1_Send_PID(320,PID_ROLL.KI,PID_ROLL.KD,1,0,0);
